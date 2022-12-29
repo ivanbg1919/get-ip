ba6eca5c87ae74f745f1530006e50a7114e32bf8
Ivan <74875827+ivanbg1919@users.noreply.github.com>
2022-12-29 18:27:02 +0100

    Add files via upload

---
+++ get-ip.php
@@ -0,0 +1,25 @@
+<?php
+// whether ip is from share internet
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+        $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+    }    
+    // whether ip is from share proxy
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+        $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }    
+    // whether ip is from remote address
+    else {
+        # code..
+        {
+        $ip_address = $_SERVER['REMOTE_ADDR'];
+        }     
+    }
+?>
+
+
+
+           
+
+
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require('get-ip.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>IP Adresa</title>
+</head>
+<body>
+<h1>Tvoja IP Adresa: <?php  echo $ip_address;  ?></h1>
+</body>
+</html>
